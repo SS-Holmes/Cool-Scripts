2ef1cb7251199c29da39dbf69114f94eca6e1787
SS-Holmes <33193880+SS-Holmes@users.noreply.github.com>
2017-10-29 14:22:13 +0530

    Update TicTacToe.py

--- scripts/TicTacToe.py
+++ scripts/TicTacToe.py
@@ -5,7 +5,8 @@ tictactoe = {
              1:' ', 2:' ', 3:' ',
              4:' ', 5:' ', 6:' ',
              7:' ', 8:' ', 9:' '
-            }   # the tic-tac-toe board
+            }   
+   # the tic-tac-toe board
 
 def draw_board():
     print ("   |   |   ")
